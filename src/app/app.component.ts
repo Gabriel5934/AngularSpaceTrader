e1a4970f64cc5c8e266e53d4300cc59269950750
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-07-24 19:50:17 -0300

    Convert project to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass'],
+    standalone: true,
+    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
 })
 export class AppComponent {
   title = 'AngularSpaceTrader';
