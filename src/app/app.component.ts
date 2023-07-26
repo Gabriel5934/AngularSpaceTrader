34ad1a9d1c61080e9c143b9cf9a7e1b2a76685ba
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2023-07-26 15:23:20 -0300

    Add basic registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase, NgIf } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.sass'],
-    standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
+  standalone: true,
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, NgIf],
 })
 export class AppComponent {
   title = 'AngularSpaceTrader';
+
+  registration = {
+    faction: '',
+    name: '',
+  };
+
+  id: string | undefined = undefined;
+
+  updateRegistration(key: string, value: string) {
+    this.registration[key as keyof typeof this.registration] = value;
+  }
+
+  registerAgent() {
+    console.log(this.registration);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        symbol: this.registration.name,
+        faction: this.registration.faction,
+      }),
+    };
+
+    fetch('https://api.spacetraders.io/v2/register', options)
+      .then((response) => response.json())
+      .then((response) => (this.id = response.data.agent.accountId))
+      .catch((err) => console.error(err));
+  }
+
+  copyId() {
+    navigator.clipboard
+      .writeText(this.id ?? 'undefined')
+      .then(undefined, (err) =>
+        console.error('Async: Could not copy text: ', err)
+      );
+  }
 }
